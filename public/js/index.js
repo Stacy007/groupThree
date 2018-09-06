edea54e16342038b2e46f7a7b23f0c0cd54f5363
Philip Digiglio <37520782+PhilipDig@users.noreply.github.com>
2018-09-05 18:11:56 -0700

    uncommented auto complete

--- public/js/index.js
+++ public/js/index.js
@@ -227,12 +227,10 @@ var handleDeleteBtnClick = function() {
   });
 };
 
-/*
 // Add autocomplete functionality to recommendation
 autocomplete = new google.maps.places.Autocomplete(
   document.getElementById("item-text")
 );
-*/
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
